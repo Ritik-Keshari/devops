f4ab9bb816309c707b34d9d1c50105ae67aba03d
afser786 <afhussain20@gmail.com>
2025-11-20 01:10:34 +0530

    Fix VideoCall eslint warningsss

--- src/config.js
+++ src/config.js
@@ -3,13 +3,16 @@ const BACKEND = process.env.REACT_APP_BACKEND_URL || "https://afser.duckdns.org"
 const config = {
   BACKEND,
 
+  // USER CONTROLLER
   LOGIN: `${BACKEND}/user/login`,
   REGISTER: `${BACKEND}/user/register`,
   USER_LIST: `${BACKEND}/user/list`,
 
+  // WEBSOCKET
   WS: "https://afser.duckdns.org/ws",
 
-  AI_CHAT: `${BACKEND}/ai/chat`,
+  // AI CONTROLLER (corrected)
+  AI_CHAT: `${BACKEND}/api/ai/chat`,
 };
 
 export default config;
