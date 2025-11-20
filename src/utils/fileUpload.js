963768c5f67ef7a86a2c7765985a1c6c46238f07
afser786 <afhussain20@gmail.com>
2025-11-20 23:47:26 +0530

    add

--- src/utils/fileUpload.js
+++ src/utils/fileUpload.js
@@ -5,19 +5,20 @@ export async function uploadFileToBackend(file) {
   formData.append("file", file);
 
   try {
-    const res = await fetch(`${Config.BACKEND}/file/upload`, {
+    const res = await fetch(`${Config.BACKEND}/api/file/upload`, {
       method: "POST",
       body: formData,
     });
 
     if (!res.ok) {
-      throw new Error("Upload failed: " + res.statusText);
+      console.log("STATUS:", res.status);
+      console.log("TEXT:", await res.text());
+      throw new Error("Upload failed");
     }
 
-    // Backend should return { "url": "https://..." }
     const data = await res.json();
-
     return data.url;
+
   } catch (error) {
     console.error("File upload error:", error);
     return null;
